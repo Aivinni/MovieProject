830728e9cf61bfdb6cd82eb12e07ec9521ebac58
aivin1 <aivinni1219@gmail.com>
2024-02-15 10:39:40 -0500

    movie getter

--- Main.java
+++ Main.java
@@ -3,13 +3,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> movies = 
+        ArrayList<Movie> movies = new ArrayList<>();
         try {
             Scanner fileScanner = new Scanner(new File("movies_data.csv"));
             while (fileScanner.hasNext()) {
-                String movieData = fileScanner.next();
-                wordList.add(movieData);
+                String[] movieData = fileScanner.next().split(",");
+                Movie movie = new Movie(movieData[0], movieData[1], movieData[2], movieData[3], Integer.parseInt(movieData[4]), Integer.parseInt(movieData[5]));
+                movies.add(movie);
             }
+            fileScanner.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
