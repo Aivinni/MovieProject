184a25f7cec6fcad6214616ef8506a64947d8468
aivin1 <aivinni1219@gmail.com>
2024-02-15 10:21:12 -0500

    Movie Fin

---
+++ Movie.java
@@ -0,0 +1,36 @@
+public class Movie {
+    private String title;
+    private String cast;
+    private String director;
+    private String overview;
+    private int runtime;
+    private int rating;
+
+    public Movie(String title, String cast, String director, String overview, int runtime, int rating) {
+        this.title = title;
+        this.cast = cast;
+        this.director = director;
+        this.overview = overview;
+        this.runtime = runtime;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getCast() {
+        return cast;
+    }
+    public String getDirector() {
+        return director;
+    }
+    public String getOverview() {
+        return overview;
+    }
+    public int getRuntime() {
+        return runtime;
+    }
+    public int getRating() {
+        return rating;
+    }
+}
