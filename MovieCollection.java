2176f8ebe1ce3543250152fb646d0af2bc06be9a
BT_1N3_09 <sample@bths.edu>
2024-02-15 10:29:05 -0500

    classes added

---
+++ MovieCollection.java
@@ -0,0 +1,32 @@
+public class MovieCollection {
+    private ArrayList<String>
+
+    public MovieCollection() {
+
+    }
+
+    public void getMenu() {
+        System.out.println("Welcome to the movie collection!");
+        String menuOption = "";
+
+        while (!menuOption.equals("q")) {
+            System.out.println("------------ Main Menu ----------");
+            System.out.println("- search (t)itles");
+            System.out.println("- search (c)ast");
+            System.out.println("- (q)uit");
+            System.out.print("Enter choice: ");
+            menuOption = scanner.nextLine();
+
+            if (menuOption.equals("t")) {
+                searchTitles();
+            } else if (menuOption.equals("c")) {
+                searchCast();
+            } else if (menuOption.equals("q")) {
+                System.out.println("Goodbye!");
+            } else {
+                System.out.println("Invalid choice!");
+            }
+        }
+
+    }
+}
