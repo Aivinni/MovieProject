46710f13c74cc3483a73748e3f8d2d8b91580280
aivin1 <aivinni1219@gmail.com>
2024-02-15 10:13:46 -0500

    movies import

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        ArrayList<String> movies = 
+        try {
+            Scanner fileScanner = new Scanner(new File("movies_data.csv"));
+            while (fileScanner.hasNext()) {
+                String movieData = fileScanner.next();
+                wordList.add(movieData);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        
     }
 }
